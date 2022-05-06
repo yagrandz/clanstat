6b3e693d3c8bc3102bc5696855b4ae5e8140cd04
yagrand3z <49150955+yagrand3z@users.noreply.github.com>
2022-05-06 16:56:09 +0300

    history

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 class ClanStat {
 	constructor(u){
 		$.getScript( u )
-		  .done(this.createTable.bind(this))
+		  .done(this.onDataLoad.bind(this))
 		  .fail(function( jqxhr, settings, exception ) {
 			alert('Data load Error');
 		});
+	}
+	
+	onDataLoad(){
+		this.createTable();
+		this.createCharts();
 	}
 	
 	createTable(){
@@ -16,5 +21,119 @@ class ClanStat {
 			pageLength: 50,
 			dom:'ft',
 		} );
+	}
+	
+	createCharts(){
+		this.createChart('clan_members_power_chart', chart_data.power);
+		this.createChart('clan_members_rating_chart', chart_data.rating);
+		this.createChart('clan_members_donations_chart', chart_data.donations);
+	}
+	
+	createChart(id, data){
+		var root = am5.Root.new(id);
+		root.setThemes([
+		  am5themes_Animated.new(root)
+		]);
+		var chart = root.container.children.push(am5xy.XYChart.new(root, {
+		  panX: true,
+		  panY: true,
+		  wheelX: "panX",
+		  wheelY: "zoomX",
+		  maxTooltipDistance: 0,
+		  pinchZoomX:true
+		}));
+
+		var xAxis = chart.xAxes.push(am5xy.DateAxis.new(root, {
+		  maxDeviation: 0.2,
+		  baseInterval: {
+			timeUnit: "day",
+			count: 1
+		  },
+		  renderer: am5xy.AxisRendererX.new(root, {}),
+		  tooltip: am5.Tooltip.new(root, {})
+		}));
+
+		var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+		  renderer: am5xy.AxisRendererY.new(root, {})
+		}));
+		
+		data.forEach(u => {
+		  var series = chart.series.push(am5xy.LineSeries.new(root, {
+			name: u.name,
+			xAxis: xAxis,
+			yAxis: yAxis,
+			valueYField: "value",
+			valueXField: "date",
+			legendValueText: "{valueY}",
+			tooltip: am5.Tooltip.new(root, {
+			  pointerOrientation: "horizontal",
+			  labelText: "{valueY}"
+			})
+		  }));
+
+		  series.data.setAll(u.data);
+		  series.appear();
+		});
+
+		var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {
+		  behavior: "none"
+		}));
+		cursor.lineY.set("visible", false);
+
+
+		chart.set("scrollbarX", am5.Scrollbar.new(root, {
+		  orientation: "horizontal"
+		}));
+
+		chart.set("scrollbarY", am5.Scrollbar.new(root, {
+		  orientation: "vertical"
+		}));
+
+
+		var legend = chart.rightAxesContainer.children.push(am5.Legend.new(root, {
+		  width: 200,
+		  paddingLeft: 15,
+		  height: am5.percent(100)
+		}));
+
+		legend.itemContainers.template.events.on("pointerover", function(e) {
+		  var itemContainer = e.target;
+		  var series = itemContainer.dataItem.dataContext;
+
+		  chart.series.each(function(chartSeries) {
+			if (chartSeries != series) {
+			  chartSeries.strokes.template.setAll({
+				strokeOpacity: 0.15,
+				stroke: am5.color(0x000000)
+			  });
+			} else {
+			  chartSeries.strokes.template.setAll({
+				strokeWidth: 3
+			  });
+			}
+		  })
+		})
+
+		legend.itemContainers.template.events.on("pointerout", function(e) {
+		  var itemContainer = e.target;
+		  var series = itemContainer.dataItem.dataContext;
+
+		  chart.series.each(function(chartSeries) {
+			chartSeries.strokes.template.setAll({
+			  strokeOpacity: 1,
+			  strokeWidth: 1,
+			  stroke: chartSeries.get("fill")
+			});
+		  });
+		})
+
+		legend.itemContainers.template.set("width", am5.p100);
+		legend.valueLabels.template.setAll({
+		  width: am5.p100,
+		  textAlign: "right"
+		});
+		
+		legend.data.setAll(chart.series.values);
+		chart.appear(1000, 100);
 	}
 }
